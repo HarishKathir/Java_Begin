861fb2ab2c06f5d523777c98a35142bbe96fd596
HarishKathir <harishbabu1532002@gmail.com>
2024-07-17 14:38:38 +0530

    Initial Commit

---
+++ BeginnerPrograms/BeginnerPrograms/simple_calculator.java
@@ -0,0 +1,10 @@
+package BeginnerPrograms;
+
+import java.util.Scanner;
+
+public class simple_calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.close();;
+    }
+}
